0744d74ff2a33ce8ebae937863587d7423d66606
Christian Giessel <cgiessel@tesla.com>
2025-02-17 22:39:48 +0100

    Updated read me

--- pages/ongoing_monitoring/overview/page.py
+++ pages/ongoing_monitoring/overview/page.py
@@ -2,9 +2,8 @@ import dash_mantine_components as dmc
 
 from router import RouteConfig
 
-config = RouteConfig(is_static=True)
+config = RouteConfig(is_static=True, path_template="<test_1>/test/<test_2>")
 
 
-async def layout(**kwargs):
-    return dmc.Title("OM Overview")
-
+async def layout(test_1=None, test_2=None, **kwargs):
+    return dmc.Title(f"OM Overview {test_1} {test_2}")
